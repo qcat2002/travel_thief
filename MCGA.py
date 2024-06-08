34911f6e21e1caffd19736bdf5d78030c2e339be
Zepeng Zhao <zxz174@student.bham.ac.uk>
2024-06-08 23:27:47 +0100

    update MCGA:N-point crossover

--- MCGA.py
+++ MCGA.py
@@ -2,6 +2,7 @@ import os
 import initialisation as init
 import encode
 import random
+import time
 
 
 def get_inits(tsp_path):
@@ -38,7 +39,7 @@ def repair(prob, ind):
     # print(profit)
     # print(weight)
     ratio = sorted([(picked_id[c], profit[c]/weight[c]) for c in range(len(picked_id))], key=lambda x: x[1])
-    print(ratio)
+    # print(ratio)
     while ind.vio:
         id = ratio[0][0]
         ind.kp[id-1] = 0
@@ -46,7 +47,7 @@ def repair(prob, ind):
         ratio = ratio[1:]
     # print(ind.kp)
     # print(ind.weight, prob.W)
-    print()
+    # print()
 
 
 def evaluate(prob, item_id_at_city, ind):
@@ -78,15 +79,75 @@ def evaluate(prob, item_id_at_city, ind):
     ind.fitness = profit - (prob.W * time)
 
 
+def order_sort(sort_list, reference):
+    new_list = []
+    for elem in sort_list:
+        for index in range(len(reference)):
+            if reference[index] == elem:
+                new_list.append((elem, index))
+    return new_list
+
+def crossover(p1, p2):
+    tsp1 = p1.tour[1:]
+    tsp2 = p2.tour[1:]
+    length = len(tsp1)
+    # order crossover
+    masked = [random.choice([0, 1]) for ccc in range(length)]
+    new_tsp1 = [-1] * length
+    new_tsp2 = [-1] * length
+    b1 = []
+    b2 = []
+    for index in range(len(tsp1)):
+        if masked[index] == 1:
+            new_tsp1[index] = tsp1[index]
+            new_tsp2[index] = tsp2[index]
+        else:
+            b1.append(tsp1[index])
+            b2.append(tsp2[index])
+    b1 = sorted(order_sort(b1, tsp2), key=lambda x: x[1])
+    b2 = sorted(order_sort(b2, tsp1), key=lambda x: x[1])
+    counter = 0
+    for index in range(len(tsp1)):
+        if masked[index] == 0:
+            new_tsp1[index] = b1[counter][0]
+            new_tsp2[index] = b2[counter][0]
+            counter += 1
+    new_tsp1 = [0] + new_tsp1
+    new_tsp2 = [0] + new_tsp2
+    # N-point crossover
+    kp1 = p1.kp[:]
+    kp2 = p2.kp[:]
+    length = len(kp1)
+    crossover_points = [0] + sorted(random.sample(range(1, length), 3)) + [length]
+    new_kp1 = []
+    new_kp2 = []
+    for ii in range(len(crossover_points)-1):
+        start = crossover_points[ii]
+        end = crossover_points[ii+1]
+        if ii % 2 == 0:
+            new_kp1.extend(kp1[start:end])
+            new_kp2.extend(kp2[start:end])
+        else:
+            new_kp1.extend(kp2[start:end])
+            new_kp2.extend(kp1[start:end])
+
+    # print(kp1[crossover_points[1]:crossover_points[2]])
+    # print(new_kp2[crossover_points[1]:crossover_points[2]])
+    c1 = encode.Ind(new_tsp1, new_kp1)
+    c2 = encode.Ind(new_tsp2, new_kp2)
+    return c1, c2
+
 
 def algorithm(ttp_path, tsp_path):
+    debug = True
+    start = time.time()
     prob = init.read_ttp(ttp_path)
     tours_200 = get_inits(tsp_path)
     # for tour in tours_200:
     #     print(tour)
     # print(len(tours_200))
     pop_size = 200
-    max_gen = 100
+    max_gen = 1
     order_cross = 1
     n_point_cross = 1
     opt_mutate = 0.35
@@ -100,11 +161,18 @@ def algorithm(ttp_path, tsp_path):
                 items.append(prob.item_ID[index])
         item_id_at_city[city_id-1] = items
 
-    kp = [0] * len(prob.item_ID)
+    if debug:
+        kp = [random.choice([0, 1]) for _ in range(len(prob.item_ID))]
+    else:
+        kp = [0] * len(prob.item_ID)
 
     pop = []
     for indicator in range(pop_size):
-        ind = encode.Ind(tours_200[indicator][:], kp[:])
+        if debug:
+            random.shuffle(kp)
+            ind = encode.Ind(tours_200[indicator][:], kp[:])
+        else:
+            ind = encode.Ind(tours_200[indicator][:], kp[:])
         violate(prob, ind)
         if ind.vio:
             repair(prob, ind)
@@ -120,7 +188,6 @@ def algorithm(ttp_path, tsp_path):
         while len(parents) <= pop_size:
             candidates = random.sample(old_pop, 2)
             parents.append(max(candidates, key=lambda x: x.fitness))
-        print([ind.fitness for ind in parents])
 
         i = 0
         j = 1
@@ -128,26 +195,14 @@ def algorithm(ttp_path, tsp_path):
         while j < len(parents):
             p1 = parents[i]
             p2 = parents[j]
-            tsp1 = p1.tour[:]
-            tsp2 = p2.tour[:]
-            length = len(tsp1)
-            # order crossover
-            masked = [random.choice([0,1]) for ccc in range(length)]
-            new_tsp1 = [-1] * length
-            new_tsp2 = [-1] * length
-            b1 = []
-            b2 = []
-            for index in range(len(tsp1)):
-                if masked[index] == 1:
-                    new_tsp1[index] = tsp1[index]
-                    new_tsp2[index] = tsp2[index]
-                else:
-                    b1.append(tsp1[index])
-                    b2.append(tsp2[index])
-            # print('a', b1)
-            # print('b', b2)
+            c1, c2 = crossover(p1, p2)
+            offspring.append(c1)
+            offspring.append(c2)
             i += 2
             j += 2
+        print(len(offspring))
+    end = time.time()
+    print(str(end - start))
 
 
 if __name__ == '__main__':
